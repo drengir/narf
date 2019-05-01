6072cf364bb21c108c9cca8ef7e672e6581d8978
sybnex <sybnex@gmail.com>
2019-05-01 10:46:04 +0200

    calender with id

--- flaskr/calender.py
+++ flaskr/calender.py
@@ -33,69 +33,30 @@ class Calender():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    def get(self):
+    def get(self, cal_id = "primary"):
         if not self.creds: self.loadToken()
         service = build('calendar', 'v3', credentials=self.creds)
 
         # Call the Calendar API
         now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        print('Getting the upcoming 3 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=3, singleEvents=True,
+        events_result = service.events().list(calendarId=cal_id, timeMin=now,
+                                              maxResults=5, singleEvents=True,
                                               orderBy='startTime').execute()
         events = events_result.get('items', [])
 
         if not events: 
-            print("No events found")
+            print(cal_id[:26], "-> No events found")
             return {}
 
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
+            end   = event['end'].get('dateTime', event['end'].get('date'))
             if not "location" in event: event["location"] = "no room"
-            print(start, event['summary'], event["location"])
-        print(events)
+            print(cal_id[:26], "->", start, "-", end, event['summary'], event["location"])
         return events
 
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server()
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 2 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=2, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
 if __name__ == '__main__':
     cal = Calender()
-    cal.get()
+    cal.get("a3suuihq983gnr1k7td668lmpk@group.calendar.google.com")
+    cal.get("sagkegsn0eb8lesd8ej0gssnko@group.calendar.google.com")
