a0f191372a5d13b8a6add057b1db355a50f7fee3
yvespp <ypwebstuff@gmail.com>
2019-05-02 17:43:30 +0200

    decuple events via queue

--- flaskr/server.py
+++ flaskr/server.py
@@ -1,6 +1,8 @@
 import time
 import json
 import os
+import threading
+from queue import SimpleQueue
 
 from flask import Flask
 from flask import render_template
@@ -8,7 +10,13 @@ from flask import Response
 from googlecalendar import Calendar
 
 app = Flask(__name__)
+send_queue = SimpleQueue()
 
+def poll_loop():
+    print("Poll loop startet")
+    while True:
+        with app.app_context():
+            get_message()
 
 @app.route('/')
 def root():
@@ -19,7 +27,7 @@ def stream():
     def event_stream():
         while True:
             # wait for source data to be available, then push it
-            yield 'data: {}\n\n'.format(get_message())
+            yield 'data: {}\n\n'.format(send_queue.get())
     return Response(event_stream(), mimetype="text/event-stream")
 
 def get_message():
@@ -28,34 +36,29 @@ def get_message():
 
     # return empty page
     if batch_id is None:
-        print("batch_id is non, resetting to start page")
-        with app.app_context():
-            jinja_render = render_template('welcome.jinja')
-            data = {'time': time.ctime(time.time()), 'calendarEvents': jinja_render}
-            return json.dumps(data)
+        print("batch_id is empty, resetting to start page")
+        send_event(render_template('welcome.jinja'))
+        return
 
     calendar_id = map_nfc_to_calendar_id(batch_id)
     if calendar_id is None:
-        with app.app_context():
-            print('No mapping for id {} found'.format(batch_id))
-            jinja_render = render_template('slacktime.jinja')
-            data = {'time': time.ctime(time.time()), 'calendarEvents': jinja_render}
-            return json.dumps(data)
+        print('No mapping for id {} found'.format(batch_id))
+        send_event(render_template('slacktime.jinja'))
+        return
 
+    send_event(render_template('loading.jinja'))
     events = cal.get_events(calendar_id)
     if not events:
-        with app.app_context():
-            jinja_render = render_template('slacktime.jinja')
-    else:
-        with app.app_context():
-            jinja_render = render_template('calendar.jinja', events = events)
+        send_event(render_template('slacktime.jinja'))
+        return
+    send_event(render_template('calendar.jinja', events = events))
 
+def send_event(jinja_render):
     data = {'time': time.ctime(time.time()), 'calendarEvents': jinja_render}
-    return json.dumps(data)
+    send_queue.put(json.dumps(data))
 
 def get_calendar():
-    cal = Calendar()
-    return cal
+    return  Calendar()
 
 def get_nfc():
     use_mock = 'USE_NFC_MOCK' in os.environ and os.environ['USE_NFC_MOCK'].lower() == "true"
@@ -82,4 +85,6 @@ nfc_mapping = {
 }
 
 if __name__ == "__main__":
+    x = threading.Thread(target=poll_loop, daemon=True)
+    x.start()
     app.run(host = "0.0.0.0")
