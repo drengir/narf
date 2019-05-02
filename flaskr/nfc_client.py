dd01dddb82b9dc69d4be4466bc500406a46e68c9
yvespp <ypwebstuff@gmail.com>
2019-05-02 15:42:01 +0200

    revert nfc_client to v0.13

--- flaskr/nfc_client.py
+++ flaskr/nfc_client.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import os
-import time
-
 import nfc
+import sys
 
-VERBOSE = True
-MAX_DEVICE_COUNT = 4
-# plling period in ms
-POLLING_PERIOD = 100
-# polling timeout in ms
-POLLING_TIMEOUT = 8000
+verbose = True
+max_device_count = 16
 
 class Nfc():
 
     def __init__(self):
         self.context = None
         self.device = None
+
         try:
-            self.polling_timeout = int(os.environ['POLLING_TIMEOUT'])
-        except:
-            self.polling_timeout = POLLING_TIMEOUT
-        try:
-            self.polling_period = int(os.environ['POLLING_PERIOD'])
-        except:
-            self.polling_period = POLLING_PERIOD
-        print('Using polling_period {} and polling_timeout {}'.format(self.polling_period, self.polling_timeout))
+            self.open_device()
+        finally:
+            self.shutdown()
 
     def open_device(self):
         self.context = nfc.init()
         print("{} uses libnfc {}".format(sys.argv[0], nfc.__version__))
 
-        connstrings = nfc.list_devices(self.context, MAX_DEVICE_COUNT)
+        connstrings = nfc.list_devices(self.context, max_device_count)
         szDeviceFound = len(connstrings)
         if szDeviceFound == 0:
             raise Exception("No NFC reader found!")
@@ -51,36 +40,25 @@ class Nfc():
     def shutdown(self):
         if self.device:
             nfc.close(self.device)
-        if self.context:
-            nfc.exit(self.context)
+        nfc.exit(self.context)
 
     def get_id(self):
-        try:
-            self.open_device()
-            modulation = nfc.modulation()
-            modulation.nmt = nfc.NMT_ISO14443A
-            modulation.nbr = nfc.NBR_106
-            # List ISO14443A targets
-            target = nfc.target()
-            start_time = time.time_ns()
-            while time.time_() - start_time < self.polling_timeout * 1000:
-                target_count = nfc.initiator_select_passive_target(self.device, modulation, 0, 0, target)
-                if target_count >= 0:
-                    break
-                time.sleep(self.polling_period)
+        uid = None
+        modulation = nfc.modulation()
 
-            if (target_count >= 0):
-                if (VERBOSE):
-                    print(target_count, 'ISO14443A passive target(s) found')
-                nfc.print_nfc_target(target, VERBOSE)
-                uid_len = target.nti.nai.szUidLen
-                uid = target.nti.nai.abtUid[:uid_len]
-                print("UID byte: {}".format(uid))
-                uid = str(int.from_bytes(uid, byteorder='little'))
-                print("UID int: {}".format(uid))
-                return uid
-        except:
-            # ignore exceptions
-            return None
-        finally:
-            self.shutdown()
+        modulation.nmt = nfc.NMT_ISO14443A
+        modulation.nbr = nfc.NBR_106
+        # List ISO14443A targets
+        target = nfc.target()
+        target_count = nfc.initiator_poll_target(self.device, modulation, 1, 30, 1, target)
+        if (target_count >= 0):
+            if (verbose):
+                print(target_count, 'ISO14443A passive target(s) found')
+            nfc.print_nfc_target(target, verbose)
+            uid_len = target.nti.nai.szUidLen
+            uid = target.nti.nai.abtUid[:uid_len]
+            print("UID byte: {}".format(uid))
+            uid = str(int.from_bytes(uid, byteorder='little'))
+            print("UID int: {}".format(uid))
+
+        return uid
